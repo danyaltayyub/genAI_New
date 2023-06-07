7cea0c9530e2f82ffc20683ff0ee4315706c3d9b
danyaltayyub <danyaltayyub@gmail.com>
2023-06-07 14:21:11 +0500

    format files

--- src/App.js
+++ src/App.js
@@ -3,51 +3,51 @@ import './App.css';
 import ImageUploader from "./ImageUploader"
 function App() {
 
-// """
-// Sample code for querying Pebblely API to create a new background
-// """
-// import base64
-// import io
+  // """
+  // Sample code for querying Pebblely API to create a new background
+  // """
+  // import base64
+  // import io
 
-// import requests
-// from PIL import Image
+  // import requests
+  // from PIL import Image
 
 
-// # View sample image
-// # image = Image.open(io.BytesIO(sample_url_response.content))
-// # image.show()
+  // # View sample image
+  // # image = Image.open(io.BytesIO(sample_url_response.content))
+  // # image.show()
 
-// # Convert image to Base64
-// buffer = io.BytesIO(sample_url_response.content)
-// sample_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
+  // # Convert image to Base64
+  // buffer = io.BytesIO(sample_url_response.content)
+  // sample_base64 = base64.b64encode(buffer.getvalue()).decode('utf-8')
 
-// # Query API
-// api_url = "https://api.pebblely.com"
-// endpoint = "/create-background/v1/"
-// response = requests.post(
-//     api_url + endpoint,
-//     headers={
-//         "Content-Type": "application/json",
-//         "X-Pebblely-Access-Token": "fa9ac919-966f-49f0-9b13-25b33e9b979b",
-//     },
-//     json={
-//         "image": sample_base64,
-//         "theme": "Surprise me"
-//     }
-// )
+  // # Query API
+  // api_url = "https://api.pebblely.com"
+  // endpoint = "/create-background/v1/"
+  // response = requests.post(
+  //     api_url + endpoint,
+  //     headers={
+  //         "Content-Type": "application/json",
+  //         "X-Pebblely-Access-Token": "fa9ac919-966f-49f0-9b13-25b33e9b979b",
+  //     },
+  //     json={
+  //         "image": sample_base64,
+  //         "theme": "Surprise me"
+  //     }
+  // )
 
-// if response.ok:
-//     # Success! View image and remaining credits
-//     response_json = response.json()
-//     print(f'Remaining credits: {response_json["credits"]}')
-//     image_b64 = response_json["data"]
-//     image_encoded = image_b64.encode("utf-8")
-//     image_bytes = io.BytesIO(base64.b64decode(image_encoded))
-//     image = Image.open(image_bytes)
-//     image.show()
-// else:
-//     # Error - print error message
-//     print(response.json())
+  // if response.ok:
+  //     # Success! View image and remaining credits
+  //     response_json = response.json()
+  //     print(f'Remaining credits: {response_json["credits"]}')
+  //     image_b64 = response_json["data"]
+  //     image_encoded = image_b64.encode("utf-8")
+  //     image_bytes = io.BytesIO(base64.b64decode(image_encoded))
+  //     image = Image.open(image_bytes)
+  //     image.show()
+  // else:
+  //     # Error - print error message
+  //     print(response.json())
 
 
 
@@ -57,7 +57,7 @@ function App() {
     <div className="App">
       <h1>Pebel App</h1>
 
-      <ImageUploader/>
+      <ImageUploader />
       {/* <img alt="sample" src={logo}/> */}
     </div>
   );
--- src/ImageUploader.js
+++ src/ImageUploader.js
@@ -1,65 +1,65 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 const ImageUploader = () => {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [base64Image, setBase64Image] = useState('');
+    const [selectedImage, setSelectedImage] = useState(null);
+    const [base64Image, setBase64Image] = useState('');
 
-  const handleImageChange = (event) => {
-    const file = event.target.files[0];
-    setSelectedImage(file);
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        setSelectedImage(file);
 
-    const reader = new FileReader()
-    reader.onload = () => {
-        const base64Data = reader.result;
-        setBase64Image(base64Data)
+        const reader = new FileReader()
+        reader.onload = () => {
+            const base64Data = reader.result;
+            setBase64Image(base64Data)
+        };
+        reader.readAsDataURL(file)
     };
-    reader.readAsDataURL(file)
-  };
 
-  const checkCredits = async() => {
-    const endpoint_url = "http://api.pebblely.com/credits/v1/"
-    const headers={
-        "Content-Type": "application/json",
-        "Accept": "*/*",
-        "Connection": "keep-alive",
-        "X-Pebblely-Access-Token": "7c9dbfb1-e2ad-4c8e-9aaa-91aa1db360d2",
+    const checkCredits = async () => {
+        const endpoint_url = "http://api.pebblely.com/credits/v1/"
+        const headers = {
+            "Content-Type": "application/json",
+            "Accept": "*/*",
+            "Connection": "keep-alive",
+            "X-Pebblely-Access-Token": "7c9dbfb1-e2ad-4c8e-9aaa-91aa1db360d2",
+        }
+        axios.get(endpoint_url, { headers })
+            .then(resp => {
+                console.log("Response", resp)
+            })
+            .error(err => {
+                console.log('Error', err)
+            })
     }
-    axios.get(endpoint_url, {headers})
-    .then(resp => {
-        console.log("Response", resp)
-    })
-    .error(err => {
-        console.log('Error', err)
-    })
-  }
 
-  const sendImageToEndpoint = async(base64Data) => {
-    const endpointUrl = "https://api.pebblely.com/create-background/v1/";
-    console.log('here base64Image=====', base64Image)
+    const sendImageToEndpoint = async (base64Data) => {
+        const endpointUrl = "https://api.pebblely.com/create-background/v1/";
+        console.log('here base64Image=====', base64Image)
 
-    const headers={
-        "Content-Type": "application/json",
-        "X-Pebblely-Access-Token": "7c9dbfb1-e2ad-4c8e-9aaa-91aa1db360d2",
-    }
-    const data={
-        "image": base64Image,
-        "theme": "Surprise me"
-    }
-    axios.post(endpointUrl, data, {headers})
-    .then(res => {
-        console.log("Response",res)
-    })
-    .catch(error => {
-        console.log("Error", error)
-    })
-  };
-  return (
-    <div>
-      <input type='file' onChange={handleImageChange} />
-      <button onClick={sendImageToEndpoint}>Upload</button>
-      <br />
-      <button onClick={checkCredits}>Reterive Credits</button>
-    </div>
-  );
+        const headers = {
+            "Content-Type": "application/json",
+            "X-Pebblely-Access-Token": "7c9dbfb1-e2ad-4c8e-9aaa-91aa1db360d2",
+        }
+        const data = {
+            "image": base64Image,
+            "theme": "Surprise me"
+        }
+        axios.post(endpointUrl, data, { headers })
+            .then(res => {
+                console.log("Response", res)
+            })
+            .catch(error => {
+                console.log("Error", error)
+            })
+    };
+    return (
+        <div>
+            <input type='file' onChange={handleImageChange} />
+            <button onClick={sendImageToEndpoint}>Upload</button>
+            <br />
+            <button onClick={checkCredits}>Reterive Credits</button>
+        </div>
+    );
 };
 export default ImageUploader;
